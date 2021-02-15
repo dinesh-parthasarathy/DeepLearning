4449581f4530869bd7bc4bf7e2c526adb18533b0
hi60toga <dinesh.parthasarathy@fau.de>
2021-02-15 11:56:23 +0100

    added VGG networks

--- Ex04_Pytorch/model.py
+++ Ex04_Pytorch/model.py
@@ -4,6 +4,7 @@ import torch as torch
 import torch.optim as opt
 
 
+# ResNet
 class ResBlock(nn.Module):
     def __init__(self, channels_in, channels_out, stride):
         super().__init__()
@@ -57,3 +58,138 @@ class ResNet(nn.Module):
         x = torch.sigmoid(x)
         return x
 
+
+# VGG-6 layered networks
+class VGG6(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3)
+        self.FCN = nn.Linear(512, 2)
+        self.GlobAvg = nn.AdaptiveAvgPool2d((1, 1))
+        self.MaxPool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv2(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv3(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv4(x))
+        x = self.MaxPool(x)
+        x = self.GlobAvg(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.FCN(x)
+        x = torch.sigmoid(x)
+        return x
+
+
+class VGG6A(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        self.FCN = nn.Linear(256, 2)
+        self.GlobAvg = nn.AdaptiveAvgPool2d((1, 1))
+        self.MaxPool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv2(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv3(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv4(x))
+        x = self.MaxPool(x)
+        x = self.GlobAvg(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.FCN(x)
+        x = torch.sigmoid(x)
+        return x
+
+
+class VGG6B(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3)
+        self.FCN = nn.Linear(128, 2)
+        self.GlobAvg = nn.AdaptiveAvgPool2d((1, 1))
+        self.MaxPool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv2(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv3(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv4(x))
+        x = self.MaxPool(x)
+        x = self.GlobAvg(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.FCN(x)
+        x = torch.sigmoid(x)
+        return x
+
+
+class VGG6C(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3)
+        self.FCN = nn.Linear(128, 2)
+        self.GlobAvg = nn.AdaptiveAvgPool2d((1, 1))
+        self.MaxPool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv2(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv3(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv4(x))
+        x = self.MaxPool(x)
+        x = self.GlobAvg(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.FCN(x)
+        x = torch.sigmoid(x)
+        return x
+
+
+class VGG6D(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3)
+        self.conv4 = nn.Conv2d(32, 96, kernel_size=3)
+        self.FCN = nn.Linear(96, 2)
+        self.GlobAvg = nn.AdaptiveAvgPool2d((1, 1))
+        self.MaxPool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv2(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv3(x))
+        x = self.MaxPool(x)
+        x = F.relu(self.conv4(x))
+        x = self.MaxPool(x)
+        x = self.GlobAvg(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.FCN(x)
+        x = torch.sigmoid(x)
+        return x
